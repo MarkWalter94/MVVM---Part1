ff529513dba731ce1833856a0b1eb774696e20aa
Marco Ortali <marco.ortali@live.it>
2020-09-20 19:24:36 +0200

    Add files via upload

---
+++ MVVVMPart1/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using WalterLabs.Command;
+using WalterLabs.ViewModel;
+
+namespace MVVVMPart1
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        #region Properties
+        public string InsertedText
+        {
+            get => _insertedText;
+            set { _insertedText = value; OnPropertyChanged(); }
+        }
+        private string _insertedText;
+        #endregion
+
+        #region Commands
+        public ICommand OnOkCommand { get; private set; }
+        #endregion
+
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            OnOkCommand = new RelayCommand(OnOk, OkCanExecute);
+        }
+
+        private bool OkCanExecute(object obj)
+        {
+            return InsertedText != null && InsertedText.Trim() != "";
+        }
+        #endregion
+        private void OnOk(object obj)
+        {
+            InsertedText = "ciao";
+        }
+    }
+}
